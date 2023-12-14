94c26abc4b3e94ae8cdfa68803a54b6147b2f09b
TuongDay <tuongit368@gmail.com>
2023-12-14 17:40:48 +0700

    Update code Phan trang 2

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -36,12 +36,22 @@ namespace NhanSu.Controllers
 
 
         // Phan trang
-        public async Task<IActionResult> Index(int? page)
+        public async Task<IActionResult> Index(int? page, int? PageSize)
         {
-            var model = _context.Employee.ToList().ToPagedList(page ?? 1, 5);
+            ViewBag.PageSize = new List<SelectListItem>()
+            {
+                new SelectListItem() { Value="3", Text="3"},
+                new SelectListItem() { Value="5", Text="5"},
+                new SelectListItem() { Value="10", Text="10"},
+                new SelectListItem() { Value="15", Text="15"},
+                new SelectListItem() { Value="25", Text="25"},
+                new SelectListItem() { Value="50", Text="50"},
+            };
+            int pagesize = (PageSize ?? 3);
+            ViewBag.psize = pagesize;
+            var model = _context.Employee.ToList().ToPagedList(page ?? 1, pagesize);
             return View(model);
         }
-
         // GET: Employee/Details/5
         public async Task<IActionResult> Details(int? id)
         {
